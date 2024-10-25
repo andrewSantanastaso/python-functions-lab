3a7232a7ac83a12cd2cda7f4328479bc38ac95dc
andrewSantanastaso <andrewsantanastaso@gmail.com>
2024-10-24 20:23:43 -0400

    exercise 6

--- exercises.py
+++ exercises.py
@@ -90,3 +90,22 @@ def sum_to(n):
 
 
 print('Exercise 5:', sum_to(10))
+
+# Exercise 6: Find the Largest Number
+#
+# Write a function named `largest` that takes three integers as arguments and returns the largest of them.
+#
+# Examples:
+# largest(1, 2, 3) should return 3.
+# largest(10, 4, 2) should return 10.
+#
+# Define your function and test it with different inputs.
+def largest(*args):
+    largest = args[0]
+    for arg in args:
+        if arg > largest:
+            largest=arg
+    return largest
+
+
+print('Exercise 6:', largest(10, 4, 2))
